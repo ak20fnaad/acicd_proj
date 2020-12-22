b0486d59fb5d3e51e8d867de7966d3925d350f34
ak20fnaad <nfisseha@gmail.com>
2020-12-22 23:16:12 +0000

    test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,16 +1,16 @@
 from hello import toyou, add, subtract
 
 def setup_function(function):
-  print("Running Steup: %s" % function.__name__)
-  function.x = 10
+    print("Running Steup: %s" % function.__name__)
+    function.x = 10
   
 def teardown_function(function):
-  print("Running Teardown: %s" % function.__name__)
-  del function.x 
+    print("Running Teardown: %s" % function.__name__)
+    del function.x 
 
-def test_hello_substract():
-  assert subtract(test_hello_subtract.x) == 9
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
 
 #run to see failed test
 def test_hello_add():
-  assert add(test_hello_add.x) == 12  
+    assert add(test_hello_add.x) == 12  
